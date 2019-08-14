6641fb74e6855721b95be8015005aad6d8332165
Ziyang Hu <hu.ziyang@cantab.net>
2019-08-14 16:12:51 +0800

    bells and whistles for hyberband

--- infirunner/hyperband.py
+++ infirunner/hyperband.py
@@ -5,16 +5,21 @@ import math
 import os
 import sys
 import time
+import datetime
 
+import colorama
 import click
 import infirunner.capsule
 
 import infirunner.param as param
 from collections import namedtuple
+from colorama import Style, Fore, Back
 
 from infirunner.util import make_trial_id
 from infirunner.generator import Generator
 from infirunner.watch import ExperimentWatcher
+
+colorama.init()
 
 
 def int_ceil(x):
@@ -29,6 +34,10 @@ class ElementVerdict(enum.Enum):
     GOOD = 'good'
     BAD = 'bad'
     UNKNOWN = 'unknown'
+
+
+def log_print(*args):
+    print(Fore.LIGHTBLACK_EX + f'[{datetime.datetime.now()}]' + Style.RESET_ALL, *args, file=sys.stderr)
 
 
 # Note that the metric is always assumed to be optimized towards minimum.
@@ -87,14 +96,19 @@ class Hyperband:
 
     def pprint_brackets(self):
         for bracket_idx, bracket in enumerate(self.brackets):
-            print(f'bracket {bracket_idx}')
+            log_print(Fore.LIGHTBLACK_EX + f'bracket {bracket_idx}:')
             for round_idx, round in enumerate(bracket):
                 actives = [e for e in round if e.active]
                 dones = [e for e in round if e.metric is not None]
                 budget = round[0].budget
-                print(
-                    f'\tround {round_idx:1} each with {budget:4} budgets, '
-                    f'{len(round):4} trials, {len(actives):4} active, {len(dones):4} complete')
+                to_print = (f'\tround {round_idx:1}: {len(round):3} trials with {budget:3} budgets, ' +
+                            f'{len(actives):3} active, {len(dones):3} complete')
+                if dones:
+                    best_metric = min(e.metric for e in dones if math.isfinite(e.metric))
+                    to_print += f', {best_metric:10.4f} best'
+                else:
+                    to_print = Fore.LIGHTBLACK_EX + to_print
+                log_print(to_print)
 
     def make_brackets(self):
         brackets = []
@@ -127,6 +141,9 @@ class Hyperband:
     def is_round_complete(self, bracket_id, round_id):
         return all(e.trial is not None and not e.active for e in self.brackets[bracket_id][round_id])
 
+    def is_complete(self):
+        return all(e.trial is not None and not e.active for e in self.brackets[-1][-1])
+
     def request_trial(self):
         # if all brackets are complete, raise StopIteration
         # if caller should wait, return None
@@ -135,7 +152,6 @@ class Hyperband:
             raise StopIteration
         cur_bracket = self.brackets[self.cur_bracket_idx]
         cur_round = cur_bracket[self.cur_round_idx]
-        # with_metrics = [e for e in cur_round if e.metric is not None]
 
         inactive_without_trial = [e for e in cur_round if e.trial is None and not e.active]
         if inactive_without_trial:
@@ -145,9 +161,6 @@ class Hyperband:
                 ret.active = True
                 return ret
             else:
-                # if not self.is_round_complete(self.cur_bracket_idx, self.cur_round_idx - 1):
-                #     return None
-
                 cur_round_trial_ids = set(e.trial for e in cur_round if e.trial is not None)
                 last_round_completed_trials = [e for e in cur_bracket[self.cur_round_idx - 1]
                                                if e.metric is not None and math.isfinite(e.metric)
@@ -156,16 +169,15 @@ class Hyperband:
                     # this COULD be empty, since all elements in previous round may have failed with NaN
                     # in that case the caller should initialize a new one to run
                     last_round_completed_trials.sort(key=lambda e: e.metric)
-                    ret.trial = last_round_completed_trials[0].trial
+                    best_available_trial = last_round_completed_trials[0]
+                    log_print(Fore.LIGHTBLACK_EX + 'promote best available trial', best_available_trial.trial,
+                              best_available_trial.metric,
+                              '(worst is', last_round_completed_trials[-1].metric, ')')
+                    ret.trial = best_available_trial.trial
 
                 ret.active = True
                 # if no trial is present, the caller is responsible for filling it it
                 return ret
-
-        # if any(e.trial is None and e.active for e in cur_round):
-        #     return None
-
-        # change idx and recur
 
         if self.is_round_complete(self.cur_bracket_idx, self.cur_round_idx):
             if self.cur_round_idx == len(cur_bracket) - 1:
@@ -173,7 +185,8 @@ class Hyperband:
                 self.cur_bracket_idx += 1
             else:
                 self.cur_round_idx += 1
-            print(id(self), 'proceed to bracket', self.cur_bracket_idx, 'round', self.cur_round_idx)
+            log_print(Fore.LIGHTBLACK_EX + str(id(self)), 'proceed to bracket', self.cur_bracket_idx, 'round',
+                      self.cur_round_idx)
             return self.request_trial()
         else:
             return None
@@ -181,7 +194,10 @@ class Hyperband:
     def report_trial(self, bracket_idx, round_idx, trial, metric):
         # mark inactive, set metric
         # make verdict for all completed rounds
-        print('hyperband received report', bracket_idx, round_idx, trial, metric)
+        if math.isfinite(metric):
+            log_print('hyperband received report', bracket_idx, round_idx, trial, metric)
+        else:
+            log_print(Fore.LIGHTRED_EX + 'hyperband received report', bracket_idx, round_idx, trial, metric)
         requested_round = self.brackets[bracket_idx][round_idx]
         requested_element = None
         for el in requested_round:
@@ -194,7 +210,7 @@ class Hyperband:
 
 class HyperbandDriver:
     def __init__(self, experiment_dir, trial_generator, param_generator, min_budget, max_budget,
-                 reduction_ratio, sleep_interval, max_hyperbands):
+                 reduction_ratio, sleep_interval, max_hyperbands, mode):
         self.experiment_dir = experiment_dir
         self.min_budget = min_budget
         self.max_budget = max_budget
@@ -206,11 +222,12 @@ class HyperbandDriver:
         self.trial_generator = trial_generator
         self.param_generator = param_generator
         self.max_hyperbands = max_hyperbands
+        self.is_maximize = mode == 'maximize'
 
     def generate_new_trial(self, end_budget, n_gpu=1):
         params = self.param_generator.get_next_parameter()
         new_id = make_trial_id()
-        print(f'generate new trial {new_id} with budget 0 -> {end_budget}')
+        log_print(Fore.LIGHTGREEN_EX + f'generate new trial {new_id} with budget 0 -> {end_budget}')
         self.trial_generator.change_capsule_trial_id(new_id)
         self.trial_generator.save_start_state(start_budget=0,
                                               end_budget=end_budget,
@@ -221,7 +238,8 @@ class HyperbandDriver:
     def amend_trial(self, old_trial, end_budget, n_gpu=1):
         self.trial_generator.change_capsule_trial_id(old_trial)
         new_state = self.trial_generator.amend_start_state(end_budget=end_budget, n_gpu=n_gpu)
-        print(f'amended trial {old_trial} with budget {new_state["start_budget"]} -> {new_state["end_budget"]}')
+        log_print(
+            Fore.LIGHTBLUE_EX + f'amended trial {old_trial} with budget {new_state["start_budget"]} -> {new_state["end_budget"]}')
 
     def get_next_hyperband_trial(self):
         for hyperband_idx, hyperband in enumerate(self.hyperbands):
@@ -239,7 +257,6 @@ class HyperbandDriver:
             self.hyperbands.append(Hyperband(min_budget=self.min_budget,
                                              max_budget=self.max_budget,
                                              reduction_ratio=self.reduction_ratio))
-            print('generated new hyperband group')
             return self.get_next_hyperband_trial()
         return None, None
 
@@ -263,13 +280,15 @@ class HyperbandDriver:
             if trial.trial in watcher_result:
                 completed_trials.add(trial.trial)
                 trial_result = watcher_result[trial.trial]
-                print(f'obtained watcher result for {trial.trial}')
+                log_print(Fore.LIGHTBLACK_EX + f'obtained watcher result for {trial.trial}')
                 if trial_result['budget'] != trial.budget:
                     trial_result['metric'] = float('nan')
                 if trial_result['metric'] is None:
                     trial_result['metric'] = float('nan')
-                self.hyperbands[hyperband_idx].report_trial(trial.bracket, trial.round, trial.trial,
-                                                            trial_result['metric'])
+                metric = trial_result['metric']
+                if self.is_maximize:
+                    metric = -metric
+                self.hyperbands[hyperband_idx].report_trial(trial.bracket, trial.round, trial.trial, metric)
         self.watch_active_trials[:] = [t for t in self.watch_active_trials if t[1].trial not in completed_trials]
 
     def start_trials(self):
@@ -279,11 +298,8 @@ class HyperbandDriver:
             if new_trial is not None:
                 # launch new trial
                 # add to trials being watched
-                print(f'watching trial {new_trial.trial} of band {hyperband_idx}')
+                log_print(Fore.LIGHTBLACK_EX + f'watching trial {new_trial.trial} of band {hyperband_idx}')
                 self.watch_active_trials.append((hyperband_idx, new_trial))
-
-    def get_trial_current_budget(self, trial_id):
-        pass
 
     def start(self):
         last_watching = set()
@@ -293,9 +309,11 @@ class HyperbandDriver:
             cur_watching = set(t.trial for _, t in self.watch_active_trials)
             if last_watching != cur_watching:
                 for idx, hb in enumerate(self.hyperbands):
-                    print('----- Hyperband', idx, id(hb), '-----')
+                    log_print(Fore.LIGHTBLACK_EX + '----- Hyperband', idx, id(hb), '-----')
                     hb.pprint_brackets()
             last_watching = cur_watching
+            if len(self.hyperbands) == self.max_hyperbands and all(hb.is_complete() for hb in self.hyperbands):
+                break
             time.sleep(self.sleep_interval)
 
 
@@ -307,7 +325,8 @@ class HyperbandDriver:
 @click.option('--sleep-interval', type=float, default=10.)
 @click.option('--max-hyperbands', type=int, default=2)
 @click.option('--reduction-ratio', type=float, default=math.e)
-def run(module, exp_path, min_budget, max_budget, reduction_ratio, max_hyperbands, sleep_interval):
+@click.option('--mode', type=click.Choice(['maximize', 'minimize']), default='minimize')
+def run(module, exp_path, min_budget, max_budget, reduction_ratio, max_hyperbands, sleep_interval, mode):
     exp_path = os.path.abspath(exp_path)
     trial_gen = Generator(module, exp_path)
     param_gen = ParamGen(module)
@@ -318,7 +337,8 @@ def run(module, exp_path, min_budget, max_budget, reduction_ratio, max_hyperband
                              max_budget=max_budget,
                              reduction_ratio=reduction_ratio,
                              sleep_interval=sleep_interval,
-                             max_hyperbands=max_hyperbands)
+                             max_hyperbands=max_hyperbands,
+                             mode=mode)
     driver.start()
 
 
